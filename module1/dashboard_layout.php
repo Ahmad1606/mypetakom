d8a932c15bf016bac19abe16b44849c5b2024103
LAPTOP-DGAA34D1\IDEAPAD GAMING <yutiganu@gmail.com>
2025-05-12 23:30:16 +0800

    no message

--- module1/dashboard_layout.php
+++ module1/dashboard_layout.php
@@ -25,7 +25,13 @@ $stmt->close();
 <body>
 
 <div class="topbar">
-    <div><strong>MyPetakom</strong></div>
+    <!-- <div style="display:flex"><strong>MyPetakom</strong>
+        <span> <img src="images/logoPetakom.png" alt="logo" with="50" height="50"></span>
+    </div> -->
+    <div class="container">
+        <span class="text">MyPetakom</span>
+        <img src="images/logoPetakom.png" alt="Logo" with="65" height="65">
+    </div>
     <div class="topbar-right">
         <span><?= htmlspecialchars($Role) ?>: <?= htmlspecialchars($Name) ?></span>
         <div class="avatar"><?= strtoupper(substr($Name, 0, 1)) ?></div>
@@ -34,6 +40,10 @@ $stmt->close();
 
 <div class="navmenu">
     <a href="#" class="active">Dashboard</a>
+    <a href="">Events</a>
+    <a href="">Committees</a>
+    <a href="">Merit Application</a>
+    <a href="">QR Codes</a>
     <a href="logout.php" class="logout">Logout</a>
 </div>
 
@@ -50,7 +60,10 @@ $stmt->close();
         <a href="#">👤 User Profiles</a>
     <?php elseif ($Role === "Event advisor"): ?>
         <a href="#">👤 User Profile</a>
+        <a href="">Advisor Dashboard</a>
         <a href="#">📅 Event Management</a>
+        <a href="">Commitee Management</a>
+        <a href="">Merit application</a>
         <a href="#">🧾 QR Codes</a>
     <?php endif; ?>
 </div>
