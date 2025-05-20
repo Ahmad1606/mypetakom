a87ecca690c5b8da29e77d16ab67548616594003
LAPTOP-DGAA34D1\IDEAPAD GAMING <yutiganu@gmail.com>
2025-05-20 23:29:00 +0800

    dashboard layout

--- db/config_all.php
+++ db/config_all.php
@@ -1,6 +1,8 @@
 <?php
 // config_all.php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 if (!isset($_SESSION['UserID']) || !isset($_SESSION['Role'])) {
     header("Location: ../module1/index.php");
